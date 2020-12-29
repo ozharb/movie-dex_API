837e6d1768ee6ba80a2149cb53348fc42d12b33a
Ozzie Harb <ozharb@gmail.com>
2020-12-29 14:43:26 -0800

    added security

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const MovieDex = require('./moviedex.json');
 
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
   
   const apiToken = process.env.API_TOKEN;
